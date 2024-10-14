b73993318301ae37d49d416b01003146d9bb2788
HEKYPTO <tsunyanapat@gmail.com>
2024-10-14 23:30:55 +0700

    NEW BACKEND

--- src/libs/getHospital.tsx
+++ src/libs/getHospital.tsx
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 export default async function getHospital(id: string): Promise<HospitalJson> {
-    const response = await fetch(`https://vaccine-app-backend.vercel.app/api/v1/hospitals/${id}`);
+    const response = await fetch(`https://vaccine-app-backend-blond.vercel.app/api/v1/hospitals/${id}`);
     
     if (!response.ok) {
         throw new Error('Failed to fetch hospital details');
